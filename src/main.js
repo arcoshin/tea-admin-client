7066fb9df2a5217782d7b2d670993f154855d4e4
arcoshin <arcoshin@gmail.com>
2023-07-05 00:18:28 +0800

    tea-admin-client

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
+import zh_tw_Lang from 'element-ui/lib/locale/lang/zh-TW'; // 引入自己的
+
 import 'element-ui/lib/theme-chalk/index.css';
 /*變量名from安裝檔名稱......此名稱同npm指令名稱*/
 import axios from 'axios';
@@ -11,7 +13,9 @@ import qs from 'qs';
 // 發請求時默認會攜帶憑證相關消息以供服務器識別
 axios.defaults.withCredentials = true;
 
-Vue.use(ElementUI);
+Vue.use(ElementUI, {
+    locale: { ...zh_tw_Lang }, // 使用本地的 zh_tw_Lang 去覆蓋官方的 zh_cn_Lang
+});
 /*Vue.prototype.自定義=變量名，其中變量名只需與上述一致即可；自定義則全局使用*/
 Vue.prototype.qs = qs;
 Vue.prototype.axios = axios;
